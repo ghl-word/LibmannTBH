3b771c345fee56a428bff0fec9ca198b33821e3d
ghl-word <ens-gabriellibmann@ugv.edu.br>
2026-09-03 00:45:04 -0300

    Iniciando teste de deploy automatizado

---
+++ app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Vitoria! O GitHub Actions fez o deploy na AWS com sucesso!');
+});
+server.listen(3000, () => console.log('Servidor rodando na porta 3000'));
