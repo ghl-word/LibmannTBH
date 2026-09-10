f1e67e116a17ea945debedbe9b843c2792565318
ghl-word <ens-gabriellibmann@ugv.edu.br>
2026-09-09 22:18:51 -0300

    forceeee

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Vitoria! O GitHub Actions fez o deploy na AWS com sucesso!');
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end('<h1>Vitoria! O GitHub Actions fez o deploy na AWS com sucesso! ✅</h1>');
 });
 server.listen(3000, '0.0.0.0', () => console.log('Servidor rodando na porta 3000'));
